2f4433776ba8cc09bc2ece256649247fccc75c66
david <davidgg777@hotmail.com>
2025-01-02 11:45:35 +1100

    refactored data table working

---
+++ src/lib/components/ui/data-table-dg.svelte
@@ -0,0 +1,176 @@
+<script lang="ts" generics="TData, TValue">
+	import {
+		type ColumnDef,
+		type PaginationState,
+		type SortingState,
+    type ColumnFiltersState,
+    type VisibilityState,
+		getCoreRowModel,
+		getPaginationRowModel,
+		getSortedRowModel,
+    getFilteredRowModel,
+    
+	} from '@tanstack/table-core';
+	import { createSvelteTable, FlexRender } from '$lib/components/ui/data-table/index.js';
+	import ChevronDown from 'lucide-svelte/icons/chevron-down';
+	import * as Table from '$lib/components/ui/table/index.js';
+	import { Button } from '$lib/components/ui/button/index.js';
+	import * as DropdownMenu from '$lib/components/ui/dropdown-menu/index.js';
+	import { Input } from '$lib/components/ui/input/index.js';
+
+	type DataTableProps<TData, TValue> = {
+		columns: ColumnDef<TData, TValue>[];
+		data: TData[];
+    filterColumn: string
+	};
+
+	let { data, columns, filterColumn }: DataTableProps<TData, TValue> = $props();
+
+	let pagination = $state<PaginationState>({ pageIndex: 0, pageSize: 10 });
+	let sorting = $state<SortingState>([]);
+  let columnFilters = $state<ColumnFiltersState>([]);
+    let columnVisibility = $state<VisibilityState>({});
+
+	const table = createSvelteTable({
+		get data() {
+			return data;
+		},
+		columns,
+		getCoreRowModel: getCoreRowModel(),
+		getPaginationRowModel: getPaginationRowModel(),
+		getSortedRowModel: getSortedRowModel(),
+    getFilteredRowModel: getFilteredRowModel(),
+		onSortingChange: (updater) => {
+			if (typeof updater === 'function') {
+				sorting = updater(sorting);
+			} else {
+				sorting = updater;
+			}
+		},
+		onPaginationChange: (updater) => {
+			if (typeof updater === 'function') {
+				pagination = updater(pagination);
+			} else {
+				pagination = updater;
+			}
+		},
+    onColumnFiltersChange: (updater) => {
+      if (typeof updater === "function") {
+        columnFilters = updater(columnFilters);
+      } else {
+        columnFilters = updater;
+      }
+    },
+    onColumnVisibilityChange: (updater) => {
+      if (typeof updater === "function") {
+        columnVisibility = updater(columnVisibility);
+      } else {
+        columnVisibility = updater;
+      }
+    },
+		state: {
+			get pagination() {
+				return pagination;
+			},
+			get sorting() {
+				return sorting;
+			},
+      get columnFilters() {
+        return columnFilters;
+      },
+      get columnVisibility() {
+        return columnVisibility;
+      },
+		}
+	});
+
+  // let filterColumn = "email"
+</script>
+
+<div class="w-full">
+	<div class="flex items-center py-4">
+		<Input
+			placeholder="Filter {filterColumn}..."
+			value={(table.getColumn(filterColumn)?.getFilterValue() as string) ?? ''}
+			oninput={(e) => table.getColumn(filterColumn)?.setFilterValue(e.currentTarget.value)}
+			onchange={(e) => {
+				table.getColumn(filterColumn)?.setFilterValue(e.currentTarget.value);
+			}}
+			class="max-w-sm"
+		/>
+		<DropdownMenu.Root>
+			<DropdownMenu.Trigger>
+				{#snippet child({ props })}
+					<Button {...props} variant="outline" class="ml-auto">
+						Columns <ChevronDown class="ml-2 size-4" />
+					</Button>
+				{/snippet}
+			</DropdownMenu.Trigger>
+			<DropdownMenu.Content align="end">
+				{#each table.getAllColumns().filter((col) => col.getCanHide()) as column}
+					<DropdownMenu.CheckboxItem
+						class="capitalize"
+						controlledChecked
+						checked={column.getIsVisible()}
+						onCheckedChange={(value) => column.toggleVisibility(!!value)}
+					>
+						{column.id}
+					</DropdownMenu.CheckboxItem>
+				{/each}
+			</DropdownMenu.Content>
+		</DropdownMenu.Root>
+	</div>
+	<div class="rounded-md border">
+		<Table.Root>
+			<Table.Header>
+				{#each table.getHeaderGroups() as headerGroup (headerGroup.id)}
+					<Table.Row>
+						{#each headerGroup.headers as header (header.id)}
+							<Table.Head>
+								{#if !header.isPlaceholder}
+									<FlexRender
+										content={header.column.columnDef.header}
+										context={header.getContext()}
+									/>
+								{/if}
+							</Table.Head>
+						{/each}
+					</Table.Row>
+				{/each}
+			</Table.Header>
+			<Table.Body>
+				{#each table.getRowModel().rows as row (row.id)}
+					<Table.Row data-state={row.getIsSelected() && 'selected'}>
+						{#each row.getVisibleCells() as cell (cell.id)}
+							<Table.Cell>
+								<FlexRender content={cell.column.columnDef.cell} context={cell.getContext()} />
+							</Table.Cell>
+						{/each}
+					</Table.Row>
+				{:else}
+					<Table.Row>
+						<Table.Cell colspan={columns.length} class="h-24 text-center">No results.</Table.Cell>
+					</Table.Row>
+				{/each}
+			</Table.Body>
+		</Table.Root>
+	</div>
+	<div class="flex items-center justify-end space-x-2 py-4">
+		<Button
+			variant="outline"
+			size="sm"
+			onclick={() => table.previousPage()}
+			disabled={!table.getCanPreviousPage()}
+		>
+			Previous
+		</Button>
+		<Button
+			variant="outline"
+			size="sm"
+			onclick={() => table.nextPage()}
+			disabled={!table.getCanNextPage()}
+		>
+			Next
+		</Button>
+	</div>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,26 +1,6 @@
 <script lang="ts">
-    import ChevronDown from "lucide-svelte/icons/chevron-down";
-    import {
-     type ColumnFiltersState,
-     type PaginationState,
-     type RowSelectionState,
-     type SortingState,
-     type VisibilityState,
-     getCoreRowModel,
-     getFilteredRowModel,
-     getPaginationRowModel,
-     getSortedRowModel
-    } from "@tanstack/table-core";
-    import * as Table from "$lib/components/ui/table/index.js";
-    import { Button } from "$lib/components/ui/button/index.js";
-    import * as DropdownMenu from "$lib/components/ui/dropdown-menu/index.js";
-    import { Input } from "$lib/components/ui/input/index.js";
-    import {
-     FlexRender,
-     createSvelteTable,
-    } from "$lib/components/ui/data-table/index.js";
 
-    import DataTable from "./data-table.svelte"
+    import DataTableDg from "$lib/components/ui/data-table-dg.svelte";
 
     import {columns} from "./columns"
     
@@ -32,6 +12,6 @@
    </script>
     
     
-   <DataTable columns={columns} data={data.payments} />
+   <DataTableDg columns={columns} data={data.payments} filterColumn="email" />
 
    
