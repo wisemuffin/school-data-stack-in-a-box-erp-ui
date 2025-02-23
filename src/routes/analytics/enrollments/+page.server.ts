91a2334b95747b22afd87d531609e25cfde74304
david <davidgg777@hotmail.com>
2025-02-23 17:10:46 +1100

    feature adding type safety to api

--- src/routes/analytics/enrollments/+page.server.ts
+++ src/routes/analytics/enrollments/+page.server.ts
@@ -2,16 +2,15 @@ import type { PageServerLoad } from './$types';
 import { queryMetrics } from '$lib/api/data-analytics/data_analytics_client';
 
 export const load: PageServerLoad = async () => {
-    const query = {
+    const response = await queryMetrics({
         metrics: ['total_enrollments'],
-        // group_by: ['metric_time'],
+        group_by: ['metric_time'],
         where: null,
         start_time: '2024-01-01',
         end_time: '2024-03-01'
-    };
-
-    const response = await queryMetrics(query);
+    });
     
+    // response.results will be typed as Array<{ total_enrollments: number, metric_time: string }>
     return {
         enrollments: response.results
     };
