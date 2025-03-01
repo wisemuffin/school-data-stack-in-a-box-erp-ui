c3bbda8cbb9d3e83891a9ec602989dceada177fc
david <davidgg777@hotmail.com>
2025-03-01 17:05:39 +1100

    cleaning forms schools

--- src/routes/school-operations/schools/school-form-schema.ts
+++
@@ -1,9 +0,0 @@
-import {z} from 'zod';
-
-export const formSchema = z.object({
-    name: z.string().min(3).max(255),
-    geography_id: z.number(),
-    // id: z.number().optional(),
-});
-
-export type FormSchema = typeof formSchema;
